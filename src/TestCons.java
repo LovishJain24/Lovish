71cb624fe5b2c601b5347ae2f52d472b0c267364
vanshitlovesfootball <129713631+vanshitlovesfootball@users.noreply.github.com>
2023-04-05 09:50:22 +0530

    This is second update

---
+++ src/TestCons.java
@@ -0,0 +1,18 @@
+
+public class TestCons {
+
+	public static void main(String[] args) {
+		
+		new TestConstructor();
+		
+
+	}
+
+}
+class TestConstructor
+{
+	public TestConstructor() {
+	
+		System.out.println("hELLLO FROM CONSTRUCTOR");
+	}
+}
