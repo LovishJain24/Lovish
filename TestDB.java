c93b9c96c5a02b9150ddd1a590f4285906b72a2c
vanshitlovesfootball <129713631+vanshitlovesfootball@users.noreply.github.com>
2023-04-26 09:41:45 +0530

    Update TestDB.java
    This is new update in file usimg MY SQL

--- TestDB.java
+++ TestDB.java
@@ -1,5 +1,8 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
 
 public class TestDB {
 
@@ -10,9 +13,20 @@ public class TestDB {
 			
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		
-		Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/lovish", "root", "root");
+		Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/lovish","root", "root");
 		
 				System.out.println("Connected");
+				Statement statement=connection.createStatement();
+				System.out.println("Connected");
+				//ResultSet resultSet=statement.executeQuery("SELECT * FROM lovish.user");
+				Scanner scanner = new Scanner(System.in);
+				System.out.println("Enter data: ");
+				int id = scanner.nextInt();
+				String name = scanner.next();
+				String query = "INSERT INTO `lovish`.`user` (`id`, `name`) VALUES('"+id+"','"+name+"')";
+				statement.executeUpdate(query);
+				System.out.println("Data Inserted Successfully");
+				
 		}
 		catch (Exception e)  {
 			System.out.println(e);
